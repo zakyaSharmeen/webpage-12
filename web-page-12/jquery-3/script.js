46f857cddc69ff90469d404c14f947d3925e0dc0
zakya sharmeen <111156443+zakyaSharmeen@users.noreply.github.com>
2023-11-30 09:52:02 +0530

    Add files via upload

---
+++ web-page-12/jquery-3/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".input-field").focusout(function (){
+        var inputValue = $(this).val();
+
+        if(inputValue===""){
+            $(this).removeClass("has-value")
+        }else{
+            $(this).addClass("has-value")
+
+        }
+    });
+});
